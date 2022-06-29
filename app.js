6d86dc5353a0abd9eeecd8e36006cd2608c03c98
Kudrotey <kudrotey.khuda@madetech.com>
2022-06-29 12:50:58 +0100

    using bcrypt module

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const PORT = 3000;
 const app = express();
 
@@ -20,34 +21,38 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
 
-    newUser.save(err => {
-        if(!err) {
-            res.render("secrets");
-        } else {
-            console.log(err);
-        }
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        newUser.save(err => {
+            if(!err) {
+                res.render("secrets");
+            } else {
+                console.log(err);
+            }
+        })
     })
+    
 })
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, (err, foundUser) => {
         if(err) {
             console.log(err);
         } else {
             if(foundUser) {
-                if(foundUser.password === password) {
-                    res.render("secrets");
-                } else {
-                    console.log("Incorrect password");
-                }
+                bcrypt.compare(password, foundUser.password, (err, result) => {
+                    if(result === true) {
+                        res.render("secrets");
+                    }
+                })
             }
         }
     })
